4dd3ffe2b07836207cf36308334ffda7d0133f4e
AlessiaPassuello505 <s322062@studenti.polito.it>
2026-06-03 11:22:36 +0200

    Update model.py

--- model/model.py
+++ model/model.py
@@ -1,6 +1,37 @@
+import networkx as nx
+
 from database.DAO import DAO
 
 
 class Model:
     def __init__(self):
-        pass
+        self._graph=nx.Graph()
+        self._idMapC={}
+
+
+    def getYears(self):
+        return DAO.getAllYears()
+
+    def creaGrafo(self,annoI,annoF):
+        self._graph.clear()
+        nodi = DAO.getNodi(annoI, annoF)
+        for n in nodi:
+            self._idMapC[n.constructorId] = n
+            self._graph.add_node(n)
+
+        archi = DAO.getArchi(annoI, annoF, self._idMapC)
+        for a in archi:
+            self._graph.add_edge(a.d1, a.d2, weight=float(a.peso))
+
+
+
+
+
+
+
+
+
+    def getGraphDetails(self):
+        return len(self._graph.nodes), len(self._graph.edges)
+
+
